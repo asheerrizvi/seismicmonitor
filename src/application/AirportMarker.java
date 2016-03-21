11191792048959d4a37fc5930b7ec394904c71fa
Asheer Rizvi <asheer.vil@gmail.com>
2016-03-21 14:42:39 +0530

    Adding Files

---
+++ src/application/AirportMarker.java
@@ -0,0 +1,62 @@
+package application;
+
+import java.util.List;
+
+import de.fhpotsdam.unfolding.data.Feature;
+import de.fhpotsdam.unfolding.data.PointFeature;
+import de.fhpotsdam.unfolding.marker.SimpleLinesMarker;
+import processing.core.PConstants;
+import processing.core.PGraphics;
+
+/** 
+ * A class to represent AirportMarkers on a world map.
+ *   
+ * @author Adam Setters and the UC San Diego Intermediate Software Development
+ * MOOC team
+ *
+ */
+public class AirportMarker extends CommonMarker {
+	public static List<SimpleLinesMarker> routes;
+	
+	public AirportMarker(Feature city) {
+		super(((PointFeature)city).getLocation(), city.getProperties());
+	
+	}
+	
+	@Override
+	public void drawMarker(PGraphics pg, float x, float y) {
+		pg.fill(11);
+		pg.ellipse(x, y, 5, 5);
+		
+		
+	}
+
+	@Override
+	public void showTitle(PGraphics pg, float x, float y) {
+		 // show rectangle with title
+		String name = getCity() + " " + getCountry() + " ";
+		pg.pushStyle();
+		
+		pg.fill(255, 255, 255);
+		pg.textSize(12);
+		pg.rectMode(PConstants.CORNER);
+		pg.rect(x, y-5-39, pg.textWidth(name) + 6, 39);
+		pg.fill(0, 0, 0);
+		pg.textAlign(PConstants.LEFT, PConstants.TOP);
+		pg.text(name, x+3, y-5-33);
+		
+		pg.popStyle();
+		// show routes
+	}
+	
+	private String getCity()
+	{
+		return getStringProperty("name");
+	}
+	
+	private String getCountry()
+	{
+		return getStringProperty("country");
+	}
+	
+}
